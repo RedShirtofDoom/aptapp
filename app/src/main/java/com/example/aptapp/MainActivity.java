bd5c06b8dcb32673203cb88eb8a7878ba60a71bc
JustAnotherRedShirt <casey.conner@mavs.uta.edu>
2019-07-11 18:20:28 -0500

    Login & Register Activity Added

--- app/src/main/java/com/example/aptapp/MainActivity.java
+++ app/src/main/java/com/example/aptapp/MainActivity.java
@@ -2,13 +2,32 @@ package com.example.aptapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Button registerButton_LoginScreen;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        registerButton_LoginScreen = findViewById(R.id.registerButton);
+
+        registerButton_LoginScreen.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                openRegisterScreen();
+            }
+        });
+    }
+
+    public void openRegisterScreen(){
+
+        //Intent intent = new Intent(this, )
     }
 }
