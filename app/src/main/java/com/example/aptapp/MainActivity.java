df3c0fc0a79b6799abcf57701faccc0629a220fa
jxm2012 <jeswinmathewkk@gmail.com>
2019-08-05 17:38:55 -0500

    attempt to allow users to add video or image in a post.

--- app/src/main/java/com/example/aptapp/MainActivity.java
+++ app/src/main/java/com/example/aptapp/MainActivity.java
@@ -3,7 +3,9 @@ package com.example.aptapp;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.provider.MediaStore;
 import android.view.View;
 import android.widget.Button;
 
@@ -30,6 +32,12 @@ public class MainActivity extends AppCompatActivity {
         });
 
         openGoogleMaps();
+
+
+        Intent pickIntent = new Intent(Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
+        pickIntent.setType("image/* video/*");
+       // startActivityForResult(pickIntent,IMAGE_PICKER_SELECT);
+
     }
 
     public void openRegisterScreen(){
@@ -40,6 +48,22 @@ public class MainActivity extends AppCompatActivity {
 
     }
 
+
+    public void onActivityResult (int requestCode, int resultCode, Intent data)
+    {
+        if(resultCode == RESULT_OK)
+        {
+            Uri selectedMediaUri = data.getData();
+            if(selectedMediaUri.toString().contains("image"))
+            {
+                //open image
+            }
+            else if(selectedMediaUri.toString().contains("video"))
+            {
+                //open video
+            }
+        }
+    }
 
     public void openGoogleMaps(){
         Intent intent2 = new Intent (this,Map_apartment.class);
